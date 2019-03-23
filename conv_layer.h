f922bee82743f4634e3c463287488b53af9f9bd8
krishnansiddharth <33415282+krishnansiddharth@users.noreply.github.com>
2019-03-23 18:32:13 +0530

    Add files via upload

--- conv_layer.h
+++ conv_layer.h
@@ -13,7 +13,28 @@ Output:
 Feature map
 
 */
+float input[]  = {{1.0,4.0,3.5},{5.0,2.65,7.95},{4.1,8.23,7.99}};
+float filter[] = {{5.0,3.1},{9.9,8.0},{11.5,9.5}};
+float output[size_i[2]-size_f[2]][size_i[1]-size_f[1]][size_i[0]-size_f[0]];
 
-int convlayer(){
-	return 0;
+float convlayer(){
+	for i=0;i<size_i[0]-size_f[0];i++
+	{
+		for j<0;j<size_i[1]-size_f[1];j++
+		{
+			for k<0;k<size_i[2]-size_f[2];k++
+			{
+				for l=0;l<size_f[0];i++
+				{
+					for m=0;l<size_f[1];i++
+					{
+						for n=0;l<size_f[1];i++
+							ouput[i][j][k] = input[n+k][m+j][i+l]*filter[n][m][l];
+					}
+				}
+			}
+		}
+	}
+
 }
+
